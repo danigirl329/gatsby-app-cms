455d83346b7c73cc4d33551f5e2c23c31d688477
danigirl329 <danigreaves@gmail.com>
2021-02-07 12:38:05 -0500

    Style about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,11 @@ import Layout from "../components/layout"
 const AboutPage = () => {
   return (
     <Layout>
-      Let's build an about page
+      <div className="centered-container">
+        <h1>Herp Derpsum</h1>
+        <p>Herp derpsum me herpem derperker ler? Derpler sherpus berps, ler de. Tee herpderpsmer, er perp ner derperker. Perp tee derp herp terpus ner sherp derperker. Derpler derpsum derperker mer, berps perp serp herpem cerp. Sherpus derpler zerpus berp herpem se sherper derperker. Sherpus er merp herpderpsmer herpem. Serp derps herpy herp, derperker terpus derp me herpsum. Perper berps der herp derpler zerpus merp herpsum jerpy? Herp derpus serp pee se perp sherpus derp nerpy. Herpem perp, derpsum cerp herpderpsmer herpsum. Me perp herpy derpsum derpy.</p>
+        <p>Derpler ze der terpus dee derpus, herpem sherlamer derp sherpus. Herderder terp tee merp ler! Sherper tee derpus herpler ter cerp derpsum mer sherp. Terpus berps le, sherper serp. Tee ner merpus herpy? Er ze le nerpy herp zerpus sherp sherlamer re tee. Ner er der sherpus le se herpsum. Herpsum herpler nerpy derpsum derperker ner derps, merpus er! Der dee se ler le. Se derpler sherper herpderpsmer der berps. Sherpus zerpus, perper sherlamer berps.</p>
+      </div>
     </Layout>
   )
 }
